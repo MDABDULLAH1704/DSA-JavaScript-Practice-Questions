14eff6ffab58bcd8bbd15b200d5cdc360ff990bf
MDABDULLAH1704 <mdabdullahqais123@gmail.com>
2025-08-26 14:49:38 +0530

    string-characterArray

---
+++ 3. DSA/24. Lecture-28 (Strings & Character Arrays - Part 1)/1script.js
@@ -0,0 +1,18 @@
+// ASCII value of A-Z : 65-90
+// ASCII value of a-z : 97-122           
+
+// Reverse an String         
+function charStrReverse(str) {
+    let arr = str.split('');
+    let st = 0;
+    let end = arr.length - 1;
+    while (st < end) {
+        [arr[st], arr[end]] = [arr[end], arr[st]];
+        st++;
+        end--;
+    }
+    return arr.join('');
+}
+console.log(charStrReverse('hello world 2'));
+
+// Another method exist to solve this question, you have to take temp (temporary) variable then swap and also using destructuring.       
